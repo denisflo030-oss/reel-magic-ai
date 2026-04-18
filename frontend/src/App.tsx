3e4ed3582829286219b6545bc098bd0d99f9fc98
denisflo030-oss <denisflo030@gmail.com>
2026-04-18 15:27:22 +0000

    fix render build - update build script and tsconfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -80,26 +80,3 @@ export default function App() {
     </div>
   );
 }
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
